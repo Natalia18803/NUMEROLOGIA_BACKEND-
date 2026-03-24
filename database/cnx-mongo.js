46b534d0c7129d4a5a0098520d21c99a62f07d77
Natalia18803 <natisss820@gmail.com>
2026-03-23 21:33:53 -0500

    fix: override db uri para que use test en vercel

--- database/cnx-mongo.js
+++ database/cnx-mongo.js
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose';
 
-const uri = process.env.MONGO_URI;
+// Interceptamos la variable de entorno de Vercel. Si dice /numerologia, la cambiamos a /test
+// (Esto soluciona el problema de que Vercel esté apuntando a la base de datos vacía)
+let uri = process.env.MONGO_URI || "mongodb+srv://Natalia:kinher1013@natalia.ksrcic0.mongodb.net/test";
+if (uri.includes('/numerologia')) {
+    uri = uri.replace('/numerologia', '/test');
+}
 
 async function conectarMongo() {
     try {
